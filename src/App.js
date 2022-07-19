58b63ac3ce682674674a6cd37103f14ec0fb4652
KimChanHo <rlacksgh0826@gmail.com>
2022-07-19 14:29:15 +0900

    #7 [2021 UPDATE] PRACTICE MOVIE APP - todo list 완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,49 @@
+import {useState, useEffect} from "react";
+
 function App() {
+  const [toDo, setTodo] = useState("");
+  const [toDos, setTodos] = useState([]);
+  const onChange = (event) => {
+      setTodo(event.target.value)
+  }
+
+  const onSubmit = (event) => {
+      event.preventDefault();
+      if(toDo === "") {
+          return;
+      }
+
+      setTodo("");
+      setTodos((prevArray) => {
+        return [toDo, ...prevArray]
+      });
+  }
+
+  console.log(toDos)
+
   return (
-    <div></div>
+    <div>
+        <h1>My To Dos ({toDos.length})</h1>
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                value={toDo}
+                onChange={onChange}
+                placeholder="write your to do..."
+            />
+            <button>
+                Add To Do
+            </button>
+        </form>
+        <hr/>
+        <ul>
+            {
+                toDos.map((item, index) => {
+                    return <li key={index}>{item}</li>
+                })
+            }
+        </ul>
+    </div>
   );
 }
 
