a32d6244b1ac2656eb14c501dac08c141c0521d1
KimChanHo <rlacksgh0826@gmail.com>
2022-07-19 09:56:13 +0900

    #7 [2021 UPDATE] PRACTICE MOVIE APP - 시작

--- src/App.js
+++ src/App.js
@@ -1,18 +1,6 @@
-import Button from "./Button";
-import Button2 from "./Button2";
-import style from "./App.module.css"
-
 function App() {
   return (
-    <div>
-        <h1 className={style.title}>Welcome back!</h1>
-        <Button
-            text={"Continue"}
-        />
-        <Button2
-            text={"Continue2"}
-        />
-    </div>
+    <div></div>
   );
 }
 
